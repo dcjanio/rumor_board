bc88bedc076ba108596e370fa816ea734ded0214
dcjanio <jan16.1.97@gmail.com>
2023-11-01 00:05:40 +0000

    changes

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,18 +1,12 @@
-const path = require('path');
 
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Loaders
-    // If you had loaders in your old config, you can push them to config.module.rules
-    // config.module.rules.push({ /* your loader config here */ });
-
-    // Plugins
-    // If you had plugins in your old config, you can push them to config.plugins
-    // config.plugins.push(new webpack.SomePlugin());
-
-    // Custom output directory (if you really need it, but usually not recommended for Next.js)
-    // config.output.path = path.resolve(__dirname, 'dist');
-
-    return config;
-  },
+export function webpack(config, { buildId, dev, isServer, defaultLoaders, webpack }) {
+  // Loaders
+  // If you had loaders in your old config, you can push them to config.module.rules
+  // config.module.rules.push({ /* your loader config here */ });
+  // Plugins
+  // If you had plugins in your old config, you can push them to config.plugins
+  // config.plugins.push(new webpack.SomePlugin());
+  // Custom output directory (if you really need it, but usually not recommended for Next.js)
+  // config.output.path = path.resolve(__dirname, 'dist');
+  return config;
 }
